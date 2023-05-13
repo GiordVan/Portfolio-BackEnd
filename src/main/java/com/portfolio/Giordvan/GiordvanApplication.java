d58036007bbdb523a1b28120f3f5622cbee1017c
GiordVan <giordvan@gmail.com>
2023-05-13 20:47:29 -0300

    Iniciación de Apache NetBeans

---
+++ src/main/java/com/portfolio/Giordvan/GiordvanApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Giordvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiordvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiordvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/Giordvan/GiordvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Giordvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiordvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
